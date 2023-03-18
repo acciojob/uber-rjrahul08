ac25537199b87cdfdf59ddee5415bcb5574b0e9d
rjrahul08 <61372250+rjrahul08@users.noreply.github.com>
2023-03-18 15:53:32 +0530

    updated controller

--- src/main/java/com/driver/controllers/AdminController.java
+++ src/main/java/com/driver/controllers/AdminController.java
@@ -15,31 +15,34 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
-
+	@Autowired
+	AdminService adminService;
 	@PostMapping("/register")
 	public ResponseEntity<Void> registerAdmin(@RequestBody Admin admin){
+		adminService.adminRegister(admin);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 	@PutMapping("/update")
 	public ResponseEntity<Admin> updateAdminPassword(@RequestParam Integer adminId, @RequestParam String password){
-		Admin updatedAdmin = new Admin();
+		Admin updatedAdmin = adminService.updatePassword(adminId,password);
 		return new ResponseEntity<>(updatedAdmin, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/delete")
 	public void deleteAdmin(@RequestParam Integer adminId){
+		adminService.deleteAdmin(adminId);
 	}
 
 	@GetMapping("/listOfCustomers")
 	public List<Customer> listOfCustomers() {
-        List<Customer> listOfCustomers = new ArrayList<>();
-        return listOfCustomers;
+		List<Customer>listOfCustomers = adminService.getListOfCustomers();
+		return listOfCustomers;
 	}
 
 	@GetMapping("/listOfDrivers")
 	public List<Driver> listOfDrivers() {
-        List<Driver> listOfDrivers = new ArrayList<>();
-        return listOfDrivers;
+		List<Driver> listOfDrivers =adminService.getListOfDrivers();
+		return listOfDrivers;
 	}
 }
